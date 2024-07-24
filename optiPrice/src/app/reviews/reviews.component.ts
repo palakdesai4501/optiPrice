4a51fdd0ee9c02369ca99bad6e90bbf58113996a
Palak <palak.desai4501@gmail.com>
2024-07-23 23:50:38 -0400

    Added ratings animation using lottie

--- optiPrice/src/app/reviews/reviews.component.ts
+++ optiPrice/src/app/reviews/reviews.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, PLATFORM_ID, Inject, HostBinding } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import lottie from 'lottie-web';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './reviews.component.html',
-  styleUrl: './reviews.component.css'
+  styleUrls: ['./reviews.component.css']
 })
-export class ReviewsComponent {
+export class ReviewsComponent implements AfterViewInit {
+  @ViewChild('lottieContainer', { static: true }) lottieContainer!: ElementRef;
+
+  @HostBinding('attr.id') componentId = 'app-reviews-component';
+
   rating: number = 3;
   numberOfComments: number | null = null;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      lottie.loadAnimation({
+        container: this.lottieContainer.nativeElement,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'assets/ratingAnimation.json' // Adjust the path to where you have stored the animation JSON file
+      });
+    }
+  }
 
   getEmoticonColor(value: number): string {
     if (value <= 1) return '#ED4337';
